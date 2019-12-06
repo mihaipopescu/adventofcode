f05859a0dcd90efb96ddfc131d0e8a86b71aaa93
Mihai Popescu <mihaipopescu@users.noreply.github.com>
2019-12-06 15:23:16 +0100

    day 3, part 2

--- 2019/03/main.go
+++ 2019/03/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -27,16 +28,23 @@ func BasenameWithoutExt(filename string) string {
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: program [INPUT_FILE]\n")
-		os.Exit(1)
-	}
+	filenamePtr := flag.String("input", "-", "Input file")
+	debugPtr := flag.Bool("pgm", false, "Generates a pgm file (useful in debugging)")
+	flag.Parse()
 
-	buf, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
+	filename := *filenamePtr
+
+	var scanner *bufio.Scanner
+
+	if filename == "-" {
+		scanner = bufio.NewScanner(os.Stdin)
+	} else {
+		buf, err := ioutil.ReadFile(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		scanner = bufio.NewScanner(strings.NewReader(string(buf)))
 	}
-	scanner := bufio.NewScanner(strings.NewReader(string(buf)))
 
 	onComma := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
 		for i := 0; i < len(data); i++ {
@@ -135,78 +143,110 @@ func main() {
 	var cx = origin.x
 	var cy = origin.y
 
-	var flag int8 = 1
+	type intersection struct {
+		point point
+		steps int
+	}
 
-	for i := 0; i < len(wires); i++ {
+	var intersections []intersection
+
+	// we run the wires instructions 3 times
+	const passes = 3
+	var wireIndexList = [3]byte{0, 1, 0}
+
+	for pass := 0; pass < passes; pass++ {
 		posX := cx
 		posY := cy
+		dist := 0
+		i := wireIndexList[pass]
+		var wireMask int8 = 1 << i
 		for j := 0; j < len(wires[i].moves); j++ {
 			m := wires[i].moves[j]
 			x1 := posX
 			x2 := posX + m.dx
-			if m.dx < 0 {
-				x2, x1 = x1, x2
-			}
+
 			y1 := posY
 			y2 := posY + m.dy
-			if m.dy < 0 {
-				y2, y1 = y1, y2
-			}
 
 			fmt.Fprintf(os.Stderr, "move %d,%d to %d,%d\n", x1, y1, x2, y2)
-			for y := y1; y <= y2; y++ {
-				for x := x1; x <= x2; x++ {
-					matrix[y][x] = matrix[y][x] | flag // wires crossing themselves don't count = logical of power of two's
+			d := 0
+			x := posX
+			y := posY
+			for k := 0; k < Abs(m.dx)+Abs(m.dy); k++ {
+				matrix[y][x] = matrix[y][x] | wireMask // wires crossing themselves don't count = logical of power of two's
+				if matrix[y][x] == 3 && dist > 0 {
+					partialDist := dist + d
+					fmt.Fprintf(os.Stderr, "Found intersection point at %d,%d with partial Dist=%d\n", y, x, partialDist)
+					intersections = append(intersections, intersection{point: point{x: x, y: y}, steps: partialDist})
+				}
+				d++
+				if m.dx < 0 {
+					x--
+				} else if m.dx > 0 {
+					x++
+				}
+				if m.dy < 0 {
+					y--
+				} else if m.dy > 0 {
+					y++
 				}
 			}
+
 			posX = posX + m.dx
 			posY = posY + m.dy
-			fmt.Fprintf(os.Stderr, "cursor is at X=%d, Y=%d\n", posX, posY)
+			dist += Abs(m.dx) + Abs(m.dy)
+			fmt.Fprintf(os.Stderr, "cursor is at X=%d, Y=%d, dist=%d\n", posX, posY, dist)
 		}
-		flag = flag << 1
 	}
 
-	var intersections []point
-
-	for i := 0; i < dimensions.y; i++ {
-		for j := 0; j < dimensions.x; j++ {
-			if matrix[i][j] == 3 {
-				fmt.Fprintf(os.Stderr, "Found intersection point at %d,%d\n", j, i)
-				intersections = append(intersections, point{x: j, y: i})
+	var uniqueIntersections []intersection
+	// de-doubling of intersections
+	for i := 0; i < len(intersections); i++ {
+		for j := i + 1; j < len(intersections); j++ {
+			if intersections[i].point == intersections[j].point {
+				fmt.Fprintf(os.Stderr, "de-doubling %d,%d\n", i, j)
+				uniqueIntersections = append(uniqueIntersections, intersection{
+					point: intersections[i].point,
+					steps: intersections[i].steps + intersections[j].steps})
 			}
 		}
 	}
 
 	minDist := math.MaxInt32
+	minSteps := math.MaxInt32
 
-	for _, p := range intersections {
-		d := Abs(p.x-cx) + Abs(p.y-cy)
+	for _, p := range uniqueIntersections {
+		d := Abs(p.point.x-cx) + Abs(p.point.y-cy)
 		if d < minDist && d > 0 {
 			minDist = d
 		}
-	}
-
-	fmt.Printf("%d\n", minDist)
-
-	//create PGM file for debugging (see https://en.wikipedia.org/wiki/Netpbm_format#PGM_example)
-
-	f, err := os.Create(fmt.Sprintf("%s.pgm", BasenameWithoutExt(os.Args[1])))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	w := bufio.NewWriter(f)
-
-	w.WriteString(fmt.Sprintf("P2\n%d %d\n3\n", dimensions.x, dimensions.y))
-
-	for i := 0; i < dimensions.y; i++ {
-		var b strings.Builder
-		for j := 0; j < dimensions.x; j++ {
-			b.WriteString(fmt.Sprintf("%d ", matrix[i][j]))
+		if p.steps < minSteps {
+			minSteps = p.steps
 		}
-		w.WriteString(fmt.Sprintf("%s\n", b.String()))
 	}
 
-	w.Flush()
+	fmt.Printf("minDist=%d\nminSteps=%d\n", minDist, minSteps)
+
+	if *debugPtr {
+		//create PGM file for debugging (see https://en.wikipedia.org/wiki/Netpbm_format#PGM_example)
+		f, err := os.Create(fmt.Sprintf("%s.pgm", BasenameWithoutExt(filename)))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		w := bufio.NewWriter(f)
+
+		w.WriteString(fmt.Sprintf("P2\n%d %d\n3\n", dimensions.x, dimensions.y))
+
+		for i := 0; i < dimensions.y; i++ {
+			var b strings.Builder
+			for j := 0; j < dimensions.x; j++ {
+				b.WriteString(fmt.Sprintf("%d ", matrix[i][j]))
+			}
+			w.WriteString(fmt.Sprintf("%s\n", b.String()))
+		}
+
+		w.Flush()
+	}
 }
